96ce85f81402b6587e1e42febf4f14d35adda9cc
RowyMonke <nattakritta_ao@cmu.ac.th>
2025-10-15 05:00:46 +0700

    remove DEF from GasbySamSi.java

--- GasbySamSi.java
+++ GasbySamSi.java
@@ -1,46 +1,68 @@
 import java.util.Random;
 
-public class GasbySamSi extends Enemy {
+public class GasbySamSi extends Enemy { //wah.. now we have just two color--
     private String color;
     private Random random = new Random();
 
+    // attack
+    protected float attackCooldown = 4f;     // Cooldown
+    protected float attackTimer = 0f;
+    protected float dashRange = 4f;          // Dash attack range (from character)
+    protected float shootRange = 7f;         // Poltergeist attack range (from character)
+    protected float dashSpeed = 6f;
+    protected boolean isDashing = false;      //All of Gasby must "Range" at first, if match the *condition* it will DASH
+
     public GasbySamSi(float x, float y, String color, Character target) {
-        super(x, y, getHP(color), getATK(color), getDEF(color), 30f, target);
+        super(x, y, getHP(color), getATK(color), 30f, target);
         this.color = color.toLowerCase();
     }
-    //We have 3 different type of Gasby -- Red, Blue & Purple
+
+    @Override
+    public void update(float delta) {
+        if (isDead) {
+            super.update(delta);
+            return;
+        }
+
+        attackTimer += delta;
+
+        if (shouldAttack()) {
+            performAttack(delta); //Poltergeist (range) or DASH (melee)
+        } else {
+            super.aiming(delta); // not in attack range -> walk to character
+        }
+
+        super.update(delta); // to animation + update bounds
+    }
+
+    protected boolean shouldAttack() {
+        return false;
+    }
+
+    protected void performAttack(float delta) {}
+
+    //We have 2 different type of Gasby -- Red & Blue
     //update Stat bt each color
     private static int getHP(String color){
         switch (color.toLowerCase()) {
-            case "blue": return 30;
-            case "red": return 30;
-            case "purple": return 60;
-            default: return 30;
+            case "blue": return 50;
+            case "red": return 50;
+            default: return 50;
         }
     }
-    private static int getATK(String color){
+    private static int getATK(String color){ //for DASH damage
         switch (color.toLowerCase()) {
-            case "blue" : return 10; //normal Gasby -> BlueGasby
-            case "red" : return 25; //RedGasby, Damage dealer gasby
-            case "purple" : return 20; //purpleGasby, more Def & HP
-            default: return 10;
-        }
-    }
-    private static int getDEF(String color){
-        switch (color.toLowerCase()) {
-            case "blue" : return 20;
-            case "red" : return 20;
-            case "purple" : return 30;
-            default: return 20;
+            case "blue" : return 15; //normal Gasby -> BlueGasby
+            case "red" : return 35; //RedGasby, Damage dealer gasby
+            default: return 15;
         }
     }
 
     /*
     for idea of this Enemy, each different color will give s.th of Character, after they're died
-    the red one gain Character HP x% from total (random chance)
-    purple will give Character DEF (random chance 50% -- random number between 1-100)/*
+    the red one gain Character HP x% from total (random chance) -- random number between 1-100)/*
      */
-    
+
     @Override
     protected void die(){
         super.die();
@@ -58,30 +80,15 @@ public class GasbySamSi extends Enemy {
                     heal(); //use in Character
                 }
                 break;
-
-            case "purple":
-                if (dropChance > 50) {
-                    gainDEF(); //use in Character
-                }
-                break;
         }
     }
 
-    private void heal(){
+    protected void heal(){
         int healAmount = (int) (target.getMaxHp() * 0.05f); //5% OF maxhp
         target.heal(healAmount);
         System.out.println("Player healed by " + healAmount + " from Red Gasby drop!");
     }
-    private void gainDEF() {
-        int defAmount = (int) (target.getMaxDEF() * 0.10f); //10% OF maxDEF
-        target.gainDEF(defAmount);
-        System.out.println("Player gained +" + defAmount + " DEF from Purple Gasby drop!");
-    }
 
-    @Override
-    public void update(float delta){
-        super.update(delta);
-    }
     @Override
     public void dispose() {}
 }
